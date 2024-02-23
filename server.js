69c487bdca94231bf2a1b2bea178be5500e1b0dc
dtscott9 <dtscott9@byui.edu>
2024-02-22 20:56:52 -0700

    added oauth

--- server.js
+++ server.js
@@ -4,7 +4,18 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongodb = require("./db/connect");
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
+const { auth, requiresAuth } = require('express-openid-connect');
+require("dotenv").config();
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
+};
 
 app
   .use(bodyParser.json())
@@ -13,7 +24,17 @@ app
     next();
   })
   .use("/", require("./routes"))
-  .use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  .use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+  .use(auth(config));
+
+app
+  .get("/", (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
+  })
+  .get("/profile", requiresAuth(), (req, res) => {
+    res.send(JSON.stringify(req.oidc.user));
+  });
+
 
 mongodb.initDb((err, mongodb) => {
   if (err) {
